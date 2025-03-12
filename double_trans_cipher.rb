aa8492484ad5743caaf9deedfdade57bfbfadb7c
Mercy1303 <mercy89313@gmail.com>
2025-03-12 15:51:22 +0800

    Implemented ModernSymmetricCipher with AES encryption and decryption

--- double_trans_cipher.rb
+++ double_trans_cipher.rb
@@ -1,15 +1,90 @@
+# frozen_string_literal: true
+
+# encrypt: Encrypts a plaintext string using double transposition.
+# decrypt: Decrypts an encrypted string back to the original plaintext.
 module DoubleTranspositionCipher
+  # Encrypts a given document using the double transposition cipher.
+  #
+  # @param document [String] The plaintext to be encrypted.
+  # @param key [Integer] The encryption key used for randomization.
+  # @return [String] The encrypted ciphertext.
   def self.encrypt(document, key)
-    # TODO: FILL THIS IN!
-    ## Suggested steps for double transposition cipher
-    # 1. find number of rows/cols such that matrix is almost square
-    # 2. break plaintext into evenly sized blocks
-    # 3. sort rows in predictibly random way using key as seed
-    # 4. sort columns of each row in predictibly random way
-    # 5. return joined cyphertext
+    _, _, matrix = prepare_matrix(document)
+
+    shuffled_rows = shuffle_rows(matrix, key)
+    transposed_matrix = shuffle_columns(shuffled_rows, key)
+
+    transposed_matrix.flatten.join
   end
 
+  # Decrypts a given ciphertext using the double transposition cipher.
+  #
+  # @param ciphertext [String] The encrypted text to be decrypted.
+  # @param key [Integer] The key used for decryption (same as encryption key).
+  # @return [String] The original plaintext.
   def self.decrypt(ciphertext, key)
-    # TODO: FILL THIS IN!
+    _, _, matrix = prepare_matrix(ciphertext)
+
+    restored_cols = reverse_shuffle_columns(matrix, key)
+    original_matrix = reverse_shuffle_rows(restored_cols, key)
+
+    original_matrix.flatten.join.rstrip
+  end
+
+  # Prepares a matrix representation of the text.
+  #
+  # @param text [String] The input string.
+  # @return [Array] The matrix (2D array) with evenly distributed characters.
+  def self.prepare_matrix(text)
+    length = text.length
+    rows = Math.sqrt(length).floor
+    cols = (length.to_f / rows).ceil
+    matrix = text.chars.each_slice(cols).to_a
+
+    # Ensure all rows have the same number of columns by padding with spaces
+    matrix.last.fill(' ', matrix.last.length...cols) if matrix.last.length < cols
+    [rows, cols, matrix]
+  end
+
+  # Shuffles the row order using the given key.
+  #
+  # @param matrix [Array<Array<String>>] The character matrix.
+  # @param key [Integer] The key used for randomization.
+  # @return [Array<Array<String>>] The row-scrambled matrix.
+  def self.shuffle_rows(matrix, key)
+    row_order = (0...matrix.size).to_a.shuffle(random: Random.new(key))
+    row_order.map { |i| matrix[i] }
+  end
+
+  # Shuffles the column order using the given key.
+  #
+  # @param matrix [Array<Array<String>>] The row-scrambled matrix.
+  # @param key [Integer] The key used for randomization.
+  # @return [Array<Array<String>>] The column-scrambled matrix.
+  def self.shuffle_columns(matrix, key)
+    col_order = (0...matrix.first.length).to_a.shuffle(random: Random.new(key))
+    matrix.map { |row| col_order.map { |j| row[j] } }
+  end
+
+  # Reverses the column shuffle to restore original order.
+  #
+  # @param matrix [Array<Array<String>>] The scrambled matrix.
+  # @param key [Integer] The key used for randomization.
+  # @return [Array<Array<String>>] The matrix with columns restored.
+  def self.reverse_shuffle_columns(matrix, key)
+    col_order = (0...matrix.first.length).to_a.shuffle(random: Random.new(key))
+    reverse_col_order = col_order.each_with_index.sort_by(&:first).map(&:last)
+    matrix.map { |row| reverse_col_order.map { |j| row[j] } }
+  end
+
+  # Reverses the row shuffle to restore original order.
+  #
+  # @param matrix [Array<Array<String>>] The matrix with restored columns.
+  # @param key [Integer] The key used for randomization.
+  # @return [Array<Array<String>>] The original matrix before encryption.
+  def self.reverse_shuffle_rows(matrix, key)
+    row_order = (0...matrix.size).to_a.shuffle(random: Random.new(key))
+    reverse_row_order = row_order.each_with_index.sort_by(&:first).map(&:last)
+    reverse_row_order.map { |i| matrix[i] }
   end
 end
--- sk_cipher.rb
+++ sk_cipher.rb
@@ -1,15 +1,57 @@
+# frozen_string_literal: true
+
+require 'openssl'
+require 'base64'
+
+# ModernSymmetricCipher module:
+# Implements AES-based encryption and decryption using a symmetric key.
+#
+# generate_new_key: Generates a new AES key encoded in Base64.
+# encrypt: Encrypts a document using AES encryption and returns a Base64 ciphertext.
+# decrypt: Decrypts a Base64 ciphertext back into its original plaintext.
 module ModernSymmetricCipher
+  ALGORITHM = 'AES-256-CBC' # Using AES with 256-bit key in CBC mode
+
+  # Generates a new 256-bit AES key and encodes it in Base64
+  #
+  # @return [String] Base64-encoded AES key
   def self.generate_new_key
-    # TODO: Return a new key as a Base64 string
+    key = OpenSSL::Cipher.new(ALGORITHM).random_key
+    Base64.strict_encode64(key) # Encode in Base64 for safe transmission
   end
 
+  # Encrypts a given document using AES encryption and returns a Base64 ciphertext
+  #
+  # @param document [String] The plaintext to be encrypted
+  # @param key [String] The Base64-encoded AES key
+  # @return [String] Base64-encoded encrypted string
   def self.encrypt(document, key)
-    # TODO: Return an encrypted string
-    #       Use base64 for ciphertext so that it is sendable as text
+    cipher = OpenSSL::Cipher.new(ALGORITHM)
+    cipher.encrypt
+    cipher.key = Base64.strict_decode64(key) # Decode Base64 key to binary
+    iv = cipher.random_iv # Generate a random IV (Initialization Vector)
+
+    encrypted = cipher.update(document) + cipher.final # Encrypt data
+    encrypted_data = iv + encrypted # Prepend IV to the encrypted data
+
+    Base64.strict_encode64(encrypted_data) # Encode result in Base64
   end
 
+  # Decrypts a given Base64-encoded ciphertext using AES decryption
+  #
+  # @param encrypted_cc [String] The Base64-encoded encrypted string
+  # @param key [String] The Base64-encoded AES key
+  # @return [String] The original plaintext
   def self.decrypt(encrypted_cc, key)
-    # TODO: Decrypt from encrypted message above
-    #       Expect Base64 encrypted message and Base64 key
+    decipher = OpenSSL::Cipher.new(ALGORITHM)
+    decipher.decrypt
+    decipher.key = Base64.strict_decode64(key) # Decode Base64 key to binary
+
+    encrypted_data = Base64.strict_decode64(encrypted_cc) # Decode Base64 ciphertext
+    iv = encrypted_data[0..15] # Extract IV (AES block size for CBC mode is 16 bytes)
+    encrypted_text = encrypted_data[16..-1] # Extract encrypted content (fix for Ruby 2.5)
+
+    decipher.iv = iv # Set IV for decryption
+    decipher.update(encrypted_text) + decipher.final # Decrypt data
   end
 end
